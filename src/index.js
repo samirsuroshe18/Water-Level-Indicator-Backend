062748a49a547d01427d3e9f08a937845f93b706
samir suroshe <sameersuroshe50@gmail.com>
2024-06-30 13:47:28 +0530

    set host name

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import app from "./app.js";
 
 
 connectDB().then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at on : https://${process.env.LOCALHOST}:${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, process.env.SERVER_HOST, ()=>{
+        console.log(`Server is running at on : https://${process.env.SERVER_HOST}:${process.env.PORT}`);
     })
 }).catch((err)=>{
     console.log('MongoDB Failed !!!', err);
